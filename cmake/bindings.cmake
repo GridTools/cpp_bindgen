f40a41cbafe2eec7b6946e508800cdec53f5e848
Hannes Vogt <hannes@havogt.de>
2019-05-24 09:44:52 +0200

    Better scoping of variables

--- cmake/bindings.cmake
+++ cmake/bindings.cmake
@@ -1,13 +1,13 @@
 function(generate_gt_bindings)
-    set(BINDINGS_SOURCE_DIR ${PROJECT_SOURCE_DIR}/src)
-    set(BINDINGS_CMAKE_DIR ${PROJECT_SOURCE_DIR}/cmake)
-    set(BINDINGS_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/include)
+    set(__C_BINDINGS_SOURCE_DIR ${PROJECT_SOURCE_DIR}/src)
+    set(__C_BINDINGS_CMAKE_DIR ${PROJECT_SOURCE_DIR}/cmake)
+    set(__C_BINDINGS_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/include)
     configure_file(cmake/gt_bindings.cmake.in
         ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/bindings_for_build/gt_bindings.cmake
         @ONLY)
-    unset(BINDINGS_SOURCE_DIR)
-    unset(BINDINGS_CMAKE_DIR)
-    unset(BINDINGS_INCLUDE_DIR)
+    unset(__C_BINDINGS_SOURCE_DIR)
+    unset(__C_BINDINGS_CMAKE_DIR)
+    unset(__C_BINDINGS_INCLUDE_DIR)
 endfunction()
 
 generate_gt_bindings()
