1fc9055ed14ecdc12c07d0f8611021b1e900ad75
Hannes Vogt <hannes@havogt.de>
2019-05-27 14:47:23 +0200

    test for make_indices

---
+++ tests/unit_tests/test_make_indices.cpp
@@ -0,0 +1,26 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2019, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <common/make_indices.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace cpp_bindgen;
+
+static_assert(std::is_same<integer_sequence<int>::value_type, int>::value, "");
+static_assert(integer_sequence<int, 1, 2, 3>::size() == 3, "");
+
+static_assert(std::is_same<make_integer_sequence<int, 3>, integer_sequence<int, 0, 1, 2>>::value, "");
+static_assert(std::is_same<make_integer_sequence<bool, 1>, integer_sequence<bool, false>>::value, "");
+
+static_assert(std::is_same<typename make_indices_c<2>::type,
+                  list<std::integral_constant<std::size_t, 0>, std::integral_constant<std::size_t, 1>>>::value,
+    "");
+
+TEST(make_indices, make_indices) {}
