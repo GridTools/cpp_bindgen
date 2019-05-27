c0b0f2e5f6993e844d64d0cd19a0f31aa8474e42
Hannes Vogt <hannes@havogt.de>
2019-05-27 15:04:10 +0200

    Update tests/unit_tests/test_make_indices.cpp
    Co-Authored-By: lukasm91 <lukasm91@users.noreply.github.com>

--- tests/unit_tests/test_make_indices.cpp
+++ tests/unit_tests/test_make_indices.cpp
@@ -17,7 +17,6 @@ static_assert(std::is_same<integer_sequence<int>::value_type, int>::value, "");
 static_assert(integer_sequence<int, 1, 2, 3>::size() == 3, "");
 
 static_assert(std::is_same<make_integer_sequence<int, 3>, integer_sequence<int, 0, 1, 2>>::value, "");
-static_assert(std::is_same<make_integer_sequence<bool, 1>, integer_sequence<bool, false>>::value, "");
 
 static_assert(std::is_same<typename make_indices_c<2>::type,
                   list<std::integral_constant<std::size_t, 0>, std::integral_constant<std::size_t, 1>>>::value,
