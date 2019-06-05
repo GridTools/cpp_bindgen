f33ff050ba23a8922ba2e8d4365ff2444e91330d
Lukas Mosimann <lmosimann@student.ethz.ch>
2019-06-05 09:57:00 +0200

    disable legacy for cuda 8

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -57,8 +57,10 @@ nice make -j8
 ./driver
 
 # test gt legacy mode
-cd ${cwd}
-mkdir -p build_legacy && cd build_legacy
-cmake .. -DCMAKE_INSTALL_PREFIX=${cwd}/install -DCPP_BINDGEN_GT_LEGACY=ON
-nice make -j8 install
-ctest .
+if [[ "$HOST" != kesch* ]]; then
+    cd ${cwd}
+    mkdir -p build_legacy && cd build_legacy
+    cmake .. -DCMAKE_INSTALL_PREFIX=${cwd}/install -DCPP_BINDGEN_GT_LEGACY=ON
+    nice make -j8 install
+    ctest .
+fi
