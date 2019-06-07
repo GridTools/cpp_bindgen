68fd9471c47df105cedf438998162f4d1d8a45cf
Lukas Mosimann <lmosimann@student.ethz.ch>
2019-06-07 11:36:47 +0200

    modify build script

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -17,14 +17,14 @@ if [[ "$HOST" == kesch* || "$HOST" == escha* ]]; then
     export BOOST_ROOT=/project/c14/install/kesch/boost/boost_1_67_0
     export CUDATOOLKIT_HOME=$CUDA_PATH
     export CUDA_ARCH=sm_37
-    SRUN_PREFIX="srun -p pp-short -c 12 --time=00:30:00"
+    RUN_PREFIX="srun -p pp-short -c 12 --time=00:30:00"
 
 elif [[ "$HOST" == tave* ]]; then
     module switch PrgEnv-cray PrgEnv-gnu
     module rm CMake
     module load /users/jenkins/easybuild/tave/modules/all/CMake/3.12.4
     export BOOST_ROOT=/project/c14/install/kesch/boost/boost_1_67_0
-    SRUN_PREFIX=""
+    RUN_PREFIX=""
 
 elif [[ "$HOST" == daint* ]]; then
     module load daint-gpu
@@ -34,7 +34,7 @@ elif [[ "$HOST" == daint* ]]; then
     module load /users/jenkins/easybuild/daint/haswell/modules/all/CMake/3.12.4
     export BOOST_ROOT=/project/c14/install/daint/boost/boost_1_67_0
     export CUDA_ARCH=sm_60
-    SRUN_PREFIX="srun -C gpu -p cscsci --account=$CSCS_ACCOUNT --time=00:30:00"
+    RUN_PREFIX="srun -C gpu -p cscsci --account=$CSCS_ACCOUNT --time=00:30:00"
 
 else
     echo "Unknown host ${HOST}, using current environment."
@@ -44,9 +44,9 @@ cwd=$(pwd)
 
 # install c_bindings
 mkdir -p build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=${cwd}/install
-nice make -j8 install
-$SRUN_PREFIX ctest .
+#cmake .. -DCMAKE_INSTALL_PREFIX=${cwd}/install
+#nice make -j8 install
+$RUN_PREFIX ctest . -vvv
 
 # compile example using the config from build tree (test for export(package))
 cd ${cwd}/example/simple
@@ -76,5 +76,5 @@ if [[ "$HOST" != kesch* ]]; then
     mkdir -p build_legacy && cd build_legacy
     cmake .. -DCMAKE_INSTALL_PREFIX=${cwd}/install -DCPP_BINDGEN_GT_LEGACY=ON
     nice make -j8 install
-    $SRUN_PREFIX ctest .
+    $RUN_PREFIX ctest . -vvv
 fi
