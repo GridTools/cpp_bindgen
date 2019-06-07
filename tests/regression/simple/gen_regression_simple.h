2a99af3d1bb4de6769e71036476678531b6e7327
Hannes Vogt <hannes@havogt.de>
2019-06-07 09:06:30 +0200

    add generated bindings

---
+++ tests/regression/simple/gen_regression_simple.h
@@ -0,0 +1,15 @@
+// This file is generated!
+#pragma once
+
+#include <cpp_bindgen/array_descriptor.h>
+#include <cpp_bindgen/handle.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void print_number_from_cpp(int);
+
+#ifdef __cplusplus
+}
+#endif
